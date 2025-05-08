b0147f1c9815264300cf1735fd0c1740222722c7
Fatlind <fatlind.isufi@student.hogent.be>
2025-05-09 00:12:26 +0200

    fixes

--- scripts/powershell/2init.ps1
+++ scripts/powershell/2init.ps1
@@ -3,17 +3,26 @@ $VM_NAAM_1, $VM_NAAM_2 = "Kwetsbare_Ubuntu_VM", "Hacker_Kali_VM"
 $RES = $false
 
 function checkVMsExcists {
-    if (Test-Path $VM_FOLDER) {
-        Write-Host "De map $VM_FOLDER bestaat al." -foregroundcolor Yellow
-        $keuze = Read-Host "`nWilt u de bestaande virtuele machines verwijderen? (j/n)"
+    param(
+        [string]$folder,
+        [string]$vm_1,
+        [string]$vm_2,
+        [bool]$vbxnet_made
+    )
+    if (Test-Path $folder) {
+        Write-Host "De map $folder bestaat al." -foregroundcolor Yellow
+        $keuze = Read-Host "`nWilt u de bestaande virtuele machines en gerelateerde settings verwijderen? (j/n)"
         if ($keuze -notin @("j", "J", "ja", "Ja", "JA")) {
             Write-Host "Afsluiten zonder wijzigingen aan te brengen."
             exit 0
         }
-        Write-Host "Verwijderen van bestaande virtuele machines..."
-        VBoxManage unregistervm $VM_NAAM_1 --delete > $null 2>&1
-        VBoxManage unregistervm $VM_NAAM_2 --delete > $null 2>&1
-        Remove-Item -Recurse -Force $VM_FOLDER > $null 2>&1
+        Write-Host "Verwijderen van bestaande virtuele machines en settings..."
+        VBoxManage unregistervm $vm_1 --delete > $null 2>&1
+        VBoxManage unregistervm $vm_2 --delete > $null 2>&1
+        if ($vbxnet_made -eq $true) {
+            VBoxManage hostonlyif remove "vboxnet0" > $null 2>&1
+        }
+        Remove-Item -Recurse -Force $folder > $null 2>&1
         if ($RES) {
             Remove-Item -Recurse -Force $(Join-Path (Join-Path $env:USERPROFILE "Downloads") "deel1.7z") > $null 2>&1
             Remove-Item -Recurse -Force $(Join-Path (Join-Path $env:USERPROFILE "Downloads") "deel2.7z") > $null 2>&1
@@ -23,7 +32,7 @@ function checkVMsExcists {
             Remove-Item -Recurse -Force $(Join-Path (Join-Path $env:HOME "Downloads") "deel2.7z") > $null 2>&1
         }
         
-        Write-Host "Bestaande virtuele machines zijn verwijderd." -ForegroundColor Green
+        Write-Host "Bestaande virtuele machines en settings zijn verwijderd." -ForegroundColor Green
         exit 0
     
     } 
@@ -37,7 +46,14 @@ else {
     $RES = $true
 }
 
-checkVMsExcists
+$LIST_ONJECTS = $(vboxmanage list hostonlyifs | where-Object { $_ -match "Name:\s+vboxnet[0-9]" })
+$VBXNET_MADE = $false
+if ($LIST_ONJECTS.Count -eq "0") {
+    VBoxManage hostonlyif create > $null 2>&1
+    $VBXNET_MADE = $true
+}
+
+checkVMsExcists -folder $VM_FOLDER -vm_1 $VM_NAAM_1 -vm2 $VM_NAAM_2 -vbxnet_made $VBXNET_MADE
 
 New-Item -Path $VM_FOLDER -ItemType Directory > $null 2>&1
 
@@ -64,13 +80,15 @@ if ($RES) {
         -OutFile $(Join-Path (Join-Path $env:USERPROFILE "Downloads") "deel2.7z") > $null 2>&1
 }
 else {
-    Write-Host "[PROGRESS] [1/6] Downloaden van eerste 7zip map..."
-    Invoke-WebRequest -Uri "https://hogent-my.sharepoint.com/:u:/g/personal/fatlind_isufi_student_hogent_be/EcHq8yP1fO5Ms5mIhfwiPkMBOemVCiv94_LffmD_j78WSQ?download=1" `
-        -OutFile $(Join-Path (Join-Path $env:HOME "Downloads") "deel1.7z") > $null 2>&1
 
     Write-Host "[PROGRESS] [2/6] Downloaden van tweede 7zip map..."
     Invoke-WebRequest -Uri "https://hogent-my.sharepoint.com/:u:/g/personal/fatlind_isufi_student_hogent_be/EbIggjbOMNpFsDRfylf9QGcB2eToYLgYh-yBIOu54u3ueA?download=1" `
         -OutFile $(Join-Path (Join-Path $env:HOME "Downloads") "deel2.7z") > $null 2>&1
+
+
+    Write-Host "[PROGRESS] [1/6] Downloaden van eerste 7zip map..."
+    Invoke-WebRequest -Uri "https://hogent-my.sharepoint.com/:u:/g/personal/fatlind_isufi_student_hogent_be/EcHq8yP1fO5Ms5mIhfwiPkMBOemVCiv94_LffmD_j78WSQ?download=1" `
+        -OutFile $(Join-Path (Join-Path $env:HOME "Downloads") "deel1.7z") > $null 2>&1
 }
 
 if ($RES) {
@@ -84,35 +102,36 @@ if ($RES) {
 else {
     Write-Host "[PROGRESS] [3/6] Toevoegen van 7zip aan de omgevingsvariabele PATH..." > $null 2>&1
     $env:PATH += ":/usr/bin/7z"
-    Write-Host "[PROGRESS] [4/6] Uitpakken van eerste 7zip map..."
-    7z x "$(Join-Path $env:HOME "Downloads" "deel1.7z")" "-o$VM_FOLDER" > $null 2>&1
     Write-Host "[PROGRESS] [5/6] Uitpakken van tweede 7zip map..."
     7z x "$(Join-Path $env:HOME "Downloads" "deel2.7z")" "-o$VM_FOLDER" > $null 2>&1
+
+    Write-Host "[PROGRESS] [4/6] Uitpakken van eerste 7zip map..."
+    7z x "$(Join-Path $env:HOME "Downloads" "deel1.7z")" "-o$VM_FOLDER" > $null 2>&1
 }
 
 Write-Host "VDI's geinstalleerd!" -ForegroundColor Green
 Write-Host "[PROGRESS] [6/6] Virtuele machines aanmaken & configureren..."
 
-VBoxManage createvm --name $VM_NAAM_1 --basefolder $VM_FOLDER --groups "/NPE_g" --ostype Ubuntu_64 --register
-VBoxManage createvm --name $VM_NAAM_2 --basefolder $VM_FOLDER --groups "/NPE_g" --ostype Debian_64 --register
+VBoxManage createvm --name $VM_NAAM_1 --basefolder $VM_FOLDER --groups "/NPE_g" --ostype Ubuntu_64 --register > $null 2>&1
+VBoxManage createvm --name $VM_NAAM_2 --basefolder $VM_FOLDER --groups "/NPE_g" --ostype Debian_64 --register > $null 2>&1
 
-VBoxManage modifyvm $VM_NAAM_1 --memory 2048 --cpus 2 --vram 64 --nic1 intnet
-VBoxManage modifyvm $VM_NAAM_2 --memory 2048 --cpus 2 --vram 64 --nic1 intnet
+VBoxManage modifyvm $VM_NAAM_1 --memory 2048 --cpus 2 --vram 64 --nic1 intnet > $null 2>&1
+VBoxManage modifyvm $VM_NAAM_2 --memory 2048 --cpus 2 --vram 64 --nic1 intnet --nic2 hostonly --hostonlyadapter2 "vboxnet0" > $null 2>&1
 
-VBoxManage storagectl $VM_NAAM_1 --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storagectl $VM_NAAM_2 --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storagectl $VM_NAAM_1 --name "SATA Controller" --add sata --controller IntelAhci > $null 2>&1
+VBoxManage storagectl $VM_NAAM_2 --name "SATA Controller" --add sata --controller IntelAhci > $null 2>&1
 
-VBoxManage storageattach $VM_NAAM_1 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM_VDI_PAD_1
-VBoxManage storageattach $VM_NAAM_2 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM_VDI_PAD_2
+VBoxManage storageattach $VM_NAAM_1 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM_VDI_PAD_1 > $null 2>&1
+VBoxManage storageattach $VM_NAAM_2 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM_VDI_PAD_2 > $null 2>&1
 
-VBoxManage modifyvm $VM_NAAM_1 --boot1 disk
-VBoxManage modifyvm $VM_NAAM_2 --boot1 disk
+VBoxManage modifyvm $VM_NAAM_1 --boot1 disk > $null 2>&1
+VBoxManage modifyvm $VM_NAAM_2 --boot1 disk > $null 2>&1
 
-VBoxManage snapshot $VM_NAAM_1 take "Init fase" --description "Gebruikersnaam: $VM_GEBRUIKERSNAAM_1 Wachtwoord: $VM_PASSWOORD_1"
-VBoxManage snapshot $VM_NAAM_2 take "Init fase" --description "Gebruikersnaam: $VM_GEBRUIKERSNAAM_2 Wachtwoord: $VM_PASSWOORD_2"
+VBoxManage snapshot $VM_NAAM_1 take "Init fase" --description "Gebruikersnaam: $VM_GEBRUIKERSNAAM_1 Wachtwoord: $VM_PASSWOORD_1" > $null 2>&1
+VBoxManage snapshot $VM_NAAM_2 take "Init fase" --description "Gebruikersnaam: $VM_GEBRUIKERSNAAM_2 Wachtwoord: $VM_PASSWOORD_2" > $null 2>&1
 
 Write-Host "Virtuele machines zijn aangemaakt!" -ForegroundColor Green
 
-VBoxManage startvm $VM_NAAM_1
-VBoxManage startvm $VM_NAAM_2
+VBoxManage startvm $VM_NAAM_1 > $null 2>&1
+VBoxManage startvm $VM_NAAM_2 > $null 2>&1
 
